5b504e273e4f1b3ad57f4bb1c0e6283eeabc89e1
fallGamlet <fallGamlet@yandex.ru>
2019-03-13 21:52:41 +0200

    Create task2.cs

---
+++ task2.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ppTest
+{
+	class MainClass
+	{
+		static void Main (string [] args)
+		{
+			MainThreadCallbackTest ();
+		}
+
+		static void MainThreadCallbackTest ()
+		{
+			PrintThreadId ("#Main");
+
+			var callback = new Action (() => {
+				PrintThreadId ("#Callback");
+			});
+
+			new Task (() => {
+				PrintThreadId ("#Task");
+				callback.Invoke ();
+			}).Start ();
+
+			Console.ReadKey ();
+		}
+
+		static void PrintThreadId (String tag)
+		{
+			Console.WriteLine ("{0} - threadId: {1}", tag, Thread.CurrentThread.ManagedThreadId);
+		}
+	}
+}
