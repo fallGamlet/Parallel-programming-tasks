9a50f4e40fc5f241959f7e99669e2beb119245d2
fallGamlet <fallgamlet@yandex.ru>
2022-03-12 16:54:03 +0200

    add comments for example linqMethodsExample

--- lesson-5-linq/Program.cs
+++ lesson-5-linq/Program.cs
@@ -3,20 +3,14 @@ using System.Text;
 
 Console.WriteLine("Linq Example Application!");
 
-
-var list = new List<int>();
-
-for (int i = 0; i < 20; i++)
-{
-    list.Add(i);
-}
+// create list with filled numbers from 0 to 20
+var list = Enumerable.Range(0, 20).ToList();
 
 printListWithForLoop(list);
 printListWithLinqFor(list);
 filterMultyPrint(list);
 filterMultyPrintOptimized(list);
 filterMultyPrintLinq(list);
-
 linqMethodsExample(list);
 
 void printListWithForLoop<T>(List<T> list) 
@@ -84,31 +78,43 @@ void filterMultyPrintLinq(List<int> list)
 }
 
 void linqMethodsExample(List<int> list) {
-
+    // check for some of list bigger then 5
     var hasNumberGreater5 = list.Any(item => item > 5);
+    // check for some of list bigger then 60
     var hasNumberGreater60 = list.Any(item => item > 60);
+    // check for all of list lower then 40
     var allNumberLower40 = list.All(item => item < 40);
+    // check for all of list lower then 10
     var allNumberLower10 = list.All(item => item < 10);
+    // get first item of list equals to 5 or throw exception
     var number5 = list.First(item => item == 5);
+    // get first item of list equals to 45 or get default value (-1)
     var number45 = list.FirstOrDefault(item => item == 45, -1);
 
+    // create secontList with translate each items of list into string by template "<{item}>"
     var secondList = list.Select(item => $"<{item}>");
+    // create thirdList with translate each items of list into negative numbers
     var thirdList = list.Select(item => -item);
     
+    // zip list with secondList and thirdList
     var resultList = list.Zip(secondList, thirdList)
+        // translate each zipped item into TrippleFelds class
         .Select(item => 
-        {
-            return new TrippleFelds<int, string, int>() {
+            new TrippleFelds<int, string, int>() {
                 Field1 = item.First,
                 Field2 = item.Second,
                 Field3 = item.Third
-            };
-        })
+            }
+        )
+        // filter items of list
         .Where(item => item.Field1 % 3 != 0)
+        // sort items of list by Field3
         .OrderBy(item => item.Field3);
 
     resultList
+        // group items of resultList
         .GroupBy(item => item.Field1 % 4)
+        // sort items of groups by key
         .OrderBy(item => item.Key)
         .ToList()
         .ForEach(group => 
@@ -118,6 +124,7 @@ void linqMethodsExample(List<int> list) {
         });
 }
 
+// Simple class for contain 3 fields
 class TrippleFelds<T1, T2, T3>
 {
     public T1 Field1 { get; set; }
