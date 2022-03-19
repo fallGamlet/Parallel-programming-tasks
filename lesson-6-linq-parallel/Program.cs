4da1cb4fa85383d31f2f36f77ed4f1b90726988d
fallGamlet <fallgamlet@yandex.ru>
2022-03-19 11:42:20 +0200

    refactoring lesson of Linq parallel

--- lesson-6-linq-parallel/Program.cs
+++ lesson-6-linq-parallel/Program.cs
@@ -7,16 +7,12 @@ var srcData = Enumerable.Range(0, 100).ToList();
 var timeStart = DateTime.Now;
 
 var dataStrList = srcData
-.AsParallel()
+.AsParallel() // make handle of collection parallele
 .Select((number) => 
 {
     Thread.Sleep(10); // Imitation of long job
     var threadId = Thread.CurrentThread.ManagedThreadId;
-    return new Result1() 
-    { 
-        key = number.ToString(), 
-        threadId = threadId 
-    };
+    return new Result1(number.ToString(), threadId);
 })
 .ToList();
 
@@ -24,7 +20,7 @@ var timeEnd = DateTime.Now;
 var duration = (timeEnd - timeStart);
 
 Console.WriteLine($"Time duration: {duration}");
-var groups = dataStrList.GroupBy(value => value.threadId);
+var groups = dataStrList.GroupBy(value => value.ThreadId);
 
 var threadIds = groups.Select(value => value.Key);
 var threadIdsStr = String.Join(", ", threadIds);
@@ -34,13 +30,19 @@ groups.ToList().ForEach(group =>
 {
     var threadId = group.Key;
     var builder = new StringBuilder();
-    var results = group.Select(value => value.key);
+    var results = group.Select(value => value.Key);
     Console.WriteLine($"{threadId} handled {results.Count()} items");
 });
 
 
 class Result1
 {
-    public string key {get; set;}
-    public int threadId {get; set;}
+    public string Key {get; private set;}
+    public int ThreadId {get; private set;}
+
+    public Result1(string key, int threadId) 
+    {
+        this.Key = key;
+        this.ThreadId = threadId;
+    }
 }
