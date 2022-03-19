68e9dc2e12c14015dfa445ce2d24a177711f75a5
fallGamlet <fallgamlet@yandex.ru>
2022-03-19 12:01:50 +0200

    add comment for lesson DataFlow

--- lesson-6-dataflow/Program.cs
+++ lesson-6-dataflow/Program.cs
@@ -5,38 +5,44 @@ using System.Threading.Tasks.Dataflow;
 
 Console.WriteLine("DataFlow example!");
 
-// runExampleForActionBlockWithSuccessfullTask();
-// runExampleForActionBlockWithFailureTask();
-// runExampleForActionBlockWithFailureTask();
+runExampleForActionBlockWithSuccessfullTask();
+runExampleForActionBlockWithFailureTask();
+runExampleForActionBlockWithFailureTask();
 runExampleForBufferBlock();
 
 void runExampleForActionBlockWithSuccessfullTask()
 {
-    var throwIfNegative = new ActionBlock<int>(n =>
+    Console.WriteLine("runExampleForActionBlockWithSuccessfullTask started");
+
+    // create action block
+    var throwIfNegative = new ActionBlock<int>(number =>
     {
-        Console.WriteLine("n = {0}", n);
-        if (n < 0)
+        Console.WriteLine("n = {0}", number);
+        if (number < 0)
         {
             throw new ArgumentOutOfRangeException();
         }
     });
 
+    // add action for execute after when action block will be completed
     throwIfNegative.Completion.ContinueWith(task =>
     {
         Console.WriteLine("The status of the completion task is '{0}'.",task.Status);
     });
 
+    // post values into action block
     throwIfNegative.Post(1);
     throwIfNegative.Post(2);
     throwIfNegative.Post(3);
     throwIfNegative.Post(4);
 
-    new Task(() => 
+    // run task for wait 1 second and mark action block as completed
+    Task.Run(() => 
     {
         Thread.Sleep(1000);
         Console.WriteLine("Tast for complete executed");
         throwIfNegative.Complete();
-    }).Start();
+    });
 
     try
     {
@@ -46,16 +52,19 @@ void runExampleForActionBlockWithSuccessfullTask()
     }
     catch (AggregateException ae)
     {
-    ae.Handle(e =>
-    {
-        Console.WriteLine("Encountered {0}: {1}", e.GetType().Name, e.Message);
-        return true;
-    });
+        // AggregateException exception is wrapper for real exception
+        ae.Handle(e =>
+        {
+            Console.WriteLine("Encountered {0}: {1}", e.GetType().Name, e.Message);
+            return true;
+        });
     }
+    Console.WriteLine("runExampleForActionBlockWithSuccessfullTask finished");
 }
 
 void runExampleForActionBlockWithFailureTask()
 {
+    Console.WriteLine("runExampleForActionBlockWithFailureTask started");
     var throwIfNegative = new ActionBlock<int>(n =>
     {
         Console.WriteLine("n = {0}", n);
@@ -72,8 +81,8 @@ void runExampleForActionBlockWithFailureTask()
 
     throwIfNegative.Post(1);
     throwIfNegative.Post(2);
-    throwIfNegative.Post(-2);
-    throwIfNegative.Post(4);
+    throwIfNegative.Post(-2); // post the value that emit exception
+    throwIfNegative.Post(4); // this value can't be apply
     throwIfNegative.Complete();
 
     try
@@ -84,21 +93,23 @@ void runExampleForActionBlockWithFailureTask()
     }
     catch (AggregateException ae)
     {
-    ae.Handle(e =>
-    {
-        Console.WriteLine("Encountered {0}: {1}", e.GetType().Name, e.Message);
-        return true;
-    });
+        ae.Handle(e =>
+        {
+            Console.WriteLine("Encountered {0}: {1}", e.GetType().Name, e.Message);
+            return true;
+        });
     }
+    Console.WriteLine("runExampleForActionBlockWithFailureTask finished");
 }
 
 void runExampleForBufferBlock() 
 {
     Console.WriteLine("runExampleForBufferBlock started");
     var options = new DataflowBlockOptions();
-    options.BoundedCapacity = 2;
+    options.BoundedCapacity = 2; // set the limit of the buffer
     var bufferBlock = new BufferBlock<int>(options);
 
+    // run task for async receive values while bufferBlock not completed
     Task.Run(() => {
         Thread.Sleep(1000);
         while (!bufferBlock.Completion.IsCompleted)
@@ -108,6 +119,7 @@ void runExampleForBufferBlock()
         }
     });
 
+    // run task for post values into bufferBlock
     Task.Run(() => {
         for (int i = 0; i < 3; i++)
         {
@@ -116,7 +128,7 @@ void runExampleForBufferBlock()
             bufferBlock.Post(i);
         }
         Thread.Sleep(2000);
-        bufferBlock.Complete();
+        bufferBlock.Complete(); // make bufferBlock as completed
     });
     
     try {
