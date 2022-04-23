c7cfb4ad2813d6262b9f24124cc89be97768b8cd
fallGamlet <fallgamlet@yandex.ru>
2022-04-23 20:58:07 +0300

    remove unused dependency

--- lesson-8-data-source/Program.cs
+++ lesson-8-data-source/Program.cs
@@ -4,6 +4,5 @@ using MyApplication;
 
 // https://api.spacexdata.com/v4/launches/latest
 Console.WriteLine("DataSource app example");
-// ILocalStorage localStorage = new LocalStorageSqlite("Data Source=database.db"); 
 var launchDataSource = new LaunchDataSourceImpl();
 var app = new App(launchDataSource);
